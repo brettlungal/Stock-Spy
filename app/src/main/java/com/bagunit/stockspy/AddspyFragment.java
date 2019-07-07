ca76fd17806911f8b4613877be1d9e23e6d03ea1
brogrammer39 <lungalb@myumanitoba.ca>
2019-07-06 22:12:46 -0500

    moarr stuff

--- app/src/main/java/com/bagunit/stockspy/AddspyFragment.java
+++ app/src/main/java/com/bagunit/stockspy/AddspyFragment.java
@@ -1,37 +1,118 @@
 package com.bagunit.stockspy;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NotificationCompat;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import org.json.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class AddspyFragment extends Fragment implements View.OnClickListener , AdapterView.OnItemSelectedListener{
 
     private TextView s;
     private Spinner dropdown;
+    private EditText tick , buff;
+    private double price , theBuff = 0;
+    private NotificationChannel channel;
+    private NotificationManager manager;
+    private FirebaseAuth mAuth;
+    private DatabaseReference dBase;
+    private boolean isTSX = true;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View addView = inflater.inflate(R.layout.fragment_addspy , container , false );
         s = (TextView)addView.findViewById(R.id.selected);
+        tick = (EditText)addView.findViewById(R.id.ticker);
         dropdown = (Spinner)addView.findViewById(R.id.selector);
+        buff = (EditText)addView.findViewById(R.id.bufferSize);
+        addView.findViewById(R.id.addTick).setOnClickListener(this);
         dropdown.setOnItemSelectedListener(this);
 
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        String databasePath = user.getEmail().substring(0,user.getEmail().indexOf("@"));
+        dBase = FirebaseDatabase.getInstance().getReference(databasePath);
+
+        channel = new NotificationChannel( "Channel1" , "Channel 1" , NotificationManager.IMPORTANCE_HIGH );
+        channel.setDescription("A stock varied outside of boundry!");
+        manager = getActivity().getSystemService(NotificationManager.class);
+        manager.createNotificationChannel(channel);
 
         return addView;
     }
 
+    private void hideKeyboard(){
+        View v = getActivity().getCurrentFocus();
+        if ( v != null ){
+            ((InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(v.getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+    }
+
     @Override
     public void onClick(View v) {
+
+        switch ( v.getId() ){
+
+            case R.id.addTick:
+
+                    hideKeyboard();
+                    String selectedTicker = tick.getText().toString().trim();
+                    theBuff = Double.parseDouble(buff.getText().toString().trim());
+                    try{
+                        if ( isTSX ){
+                            new CallbackTask().execute("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + selectedTicker + "&apikey=81PMPLMOASGL2QKM");
+                        }else {
+                            //NASDAQ api
+
+                        }
+
+                    Thread.sleep(1000);
+                }catch (InterruptedException e){
+                    //suck a dick
+                }
+                if ( !(TextUtils.isEmpty(selectedTicker)) && theBuff != 0 ){
+                    String id = dBase.push().getKey();
+                    SpyedStock stock = new SpyedStock(selectedTicker, price , theBuff , id , isTSX);
+                    dBase.child(id).setValue(stock);
+                    Toast.makeText(getActivity().getApplicationContext(),"You are now spying on "+selectedTicker,Toast.LENGTH_SHORT).show();
+                    tick.setText("");
+                    buff.setText("");
+                }
+
+                break;
+
+        }
 
     }
 
@@ -42,10 +123,12 @@ public class AddspyFragment extends Fragment implements View.OnClickListener , A
 
             case "TSX":
                 s.setText("TSX SELECTED");
+                isTSX = true;
                 break;
 
             case "NYSE":
                 s.setText("NYSE SELECTED");
+                isTSX = false;
                 break;
 
         }
@@ -56,4 +139,77 @@ public class AddspyFragment extends Fragment implements View.OnClickListener , A
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
+    public void sendNotification(){
+
+        NotificationCompat.Builder build = new NotificationCompat.Builder(getActivity().getApplicationContext() , "Channel1")
+                .setSmallIcon(R.drawable.add).setContentTitle("Stock Spy").setContentText(tick.getText().toString().trim()+" price has fluctuated outside of bounds!").setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        manager.notify(1 , build.build());
+
+    }
+
+
+    private class CallbackTask extends AsyncTask<String,Integer,String> {
+
+        private int success;
+        private StringBuilder apiResult;
+
+        protected String doInBackground(String... params){
+
+
+            try{
+                URL url = new URL(params[0]);
+                HttpsURLConnection connection = (HttpsURLConnection)url.openConnection();
+
+
+                success = connection.getResponseCode();
+
+                if ( success == 200 ){
+                    System.out.println("===============YAY SUCCESS=================");
+                }
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                apiResult = new StringBuilder();
+                String line = null;
+                while ( (line = reader.readLine()) != null ){
+                    apiResult.append(line+"\n");
+                }
+
+                String data = apiResult.toString();
+
+                JSONObject apiData = new JSONObject(data);
+                JSONObject global = apiData.getJSONObject("Global Quote");
+                if ( price == 0.0 ){
+                    //get inital price but dont change it every call
+                    price = global.getDouble("05. price");
+                }
+                System.out.println("=======\nPrice: "+price);
+
+                return apiResult.toString();
+
+            }catch(Exception e){
+                e.printStackTrace();
+                return e.toString();
+            }
+
+        }
+
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+            System.out.println(result);
+            if ( success == 200 ) {
+                s.setText(tick.getText().toString().trim()+" is trading for "+price+" per share");
+                sendNotification();
+            }else{
+                Toast.makeText(getActivity() , "api call fail" , Toast.LENGTH_SHORT).show();
+
+            }
+        }
+
+    }
+
+
 }
+
+
+
---
+++ app/src/main/java/com/bagunit/stockspy/SpyedStock.java
@@ -0,0 +1,46 @@
+package com.bagunit.stockspy;
+
+public class SpyedStock {
+
+    private String ticker , id;
+    private double staticPrice,buffer;
+    private boolean isTSX;
+
+    public SpyedStock( String ticker, double staticPrice ,  double buffer , String id , boolean isTSX){
+        this.ticker = ticker;
+        this.staticPrice = staticPrice;
+        this.buffer = buffer;
+        this.id = id;
+        this.isTSX = isTSX;
+    }
+
+    public String getTicker() {
+        return ticker;
+    }
+
+    public boolean isTSX() {
+        return isTSX;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+
+    public double getStaticPrice() {
+        return staticPrice;
+    }
+
+    public void setStaticPrice(double staticPrice) {
+        this.staticPrice = staticPrice;
+    }
+
+    public double getBuffer() {
+        return buffer;
+    }
+
+    public void setBuffer(double buffer) {
+        this.buffer = buffer;
+    }
+}
